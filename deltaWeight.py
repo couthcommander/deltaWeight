9697f75265a329dc8662a5ba2382747aa3aac574
beckca <cole.beck@vanderbilt.edu>
2016-03-07 12:18:24 -0600

    modify default timepoints from all to 100

--- deltaWeight.py
+++ deltaWeight.py
@@ -46,24 +46,20 @@ def deltas(dates, weights, myline, fh, m, d, tp=None, ts=None, wide=False):
             else:
                 dd = [str(j) for j in dd[0:nr]]
                 dw = [str(j) for j in dw[0:nr]]
-            if tp is not None:
+            if wide:
                 while len(dw) < tp:
                     dd.append(m)
                     dw.append(m)
-            if wide:
                 fh.write(d.join([myline[i]] + dw + dd) + "\n")
             else:
                 for j in range(len(dd)):
                     fh.write(d.join([myline[i]] + [dw[j], dd[j], str(j+1)]) + "\n")
     else:
-        if tp is None:
-            tp = 1
         if wide:
             ans = [m for j in range(tp)]
             fh.write(d.join([myline[0]] + ans + ans) + "\n")
         else:
-            for j in range(tp):
-                fh.write(d.join([myline[0]] + [m, m, str(j+1)]) + "\n")
+            fh.write(d.join([myline[0]] + [m, m, '1']) + "\n")
 
 def splitData(string, d):
     line = string.split(d)
@@ -76,7 +72,8 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("datafile", help='delimited file with format ValueID,PatientID,Date,Value,ETC')
     parser.add_argument("outputfile")
-    parser.add_argument("-t", "--timepoints", help='number of time points to calculate, defaults to all observations', type=int)
+    parser.add_argument("-t", "--timepoints", 
+                        help='number of time points to calculate, defaults to 100; use `0` to indicate all observations', type=int, default=100)
     parser.add_argument("-s", "--span", help='measures time points found within number of days', type=int)
     parser.add_argument("-d", "--delimiter", help='file delimiter, defaults to ","', default=',')
     parser.add_argument("-m", "--missing", help='missing value, defaults to "."', default='.')
@@ -90,9 +87,10 @@ if __name__=='__main__':
     outfile = open(args.outputfile, 'w')
     timepoints = args.timepoints
     timespan = args.span
-    if timepoints is None:
+    if timepoints == 0:
+        timepoints = None
         wideformat = False
-    elif timepoints < 1:
+    elif timepoints < 0:
         print 'timepoints must be 1 or greater'
         sys.exit(1)
     if timespan is not None and timespan < 0:
